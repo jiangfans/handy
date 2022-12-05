25131d1b4052ac7e3dc396ecd14d6a6d94296c19
jiangfan <pengweijiang@shoplazza.com>
2022-12-05 16:39:11 +0800

    feat: kafka tools

--- kafka_tools/kafka_consumer.go
+++ kafka_tools/kafka_consumer.go
@@ -12,12 +12,35 @@ type kafkaConsumer struct {
 }
 
 func (consumer *kafkaConsumer) ConsumerMsgAndBlock(ctx context.Context, f ConsumeFunc) error {
+	log.Infof("😂😂😂start receive msg ...")
+
+	var programQuitNormal bool
+	defer func() {
+		if e := recover(); e != nil {
+			if programQuitNormal {
+				log.Info("😊program quit normal")
+			} else {
+				log.Errorf("😭program quit with panic: %v", e)
+			}
+		}
+
+		_ = consumer.ConsumerGroup.Close()
+	}()
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			programQuitNormal = true
+			panic("ctx done, program quit!")
+		}
+	}()
+
 	handler := NewConsumerGroupHandler(f)
 
 	for {
 		err := consumer.ConsumerGroup.Consume(ctx, consumer.ListenTopics, handler)
 		if err != nil {
-			log.Error("program quit with error: ", err.Error())
+			log.Error("😭program quit with error: ", err.Error())
 			return err
 		}
 	}
